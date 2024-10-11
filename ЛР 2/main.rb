58c50ad503b7351635551e57004f370d767190d1
Asya <111695385+asyanix@users.noreply.github.com>
2024-10-11 19:45:34 +0300

    Зад1.6. Аргументы конструктора теперь в форме ХЭШа

--- ЛР 2/main.rb
+++ ЛР 2/main.rb
@@ -1,8 +1,8 @@
 require_relative 'student_class.rb'
 
-student1 = Student.new(id: 1, surname: "Cheuzh", name: "Asya", patronymic: "Aslanbievna", phone: "89827484999", telegram: "asyanix", email: "asya_cheuzh@gmail.com", git: "asyanix")
-student2 = Student.new(id: 2, surname: "Kirkorov", name: "Philipp", patronymic: "Bedrosovich")
-student3 = Student.new(id: 3, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "89728946758", telegram: "saintJuuuly")
+student1 = Student.new({ id: 1, surname: "Cheuzh", name: "Asya", patronymic: "Aslanbievna", phone: "89827484999", telegram: "asyanix", email: "asya_cheuzh@gmail.com", git: "asyanix"})
+student2 = Student.new({id: 2, surname: "Kirkorov", name: "Philipp", patronymic: "Bedrosovich"})
+student3 = Student.new({id: 3, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "89728946758", telegram: "saintJuuuly"})
 
 student1.show_info()
 student2.show_info()
--- ЛР 2/student_class.rb
+++ ЛР 2/student_class.rb
@@ -1,15 +1,15 @@
 class Student
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
-   def initialize(id:, surname:, name:, patronymic:, phone: nil, telegram: nil, email: nil, git: nil)
-    @id = id
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+  def initialize(params)
+    @id = params[:id]
+    @surname = params[:surname]
+    @name = params[:name]
+    @patronymic = params[:patronymic]
+    @phone = params[:phone] || nil
+    @telegram = params[:telegram] || nil
+    @email = params[:email] || nil
+    @git = params[:git] || nil
   end
 
   # Вывод информации о студенте
