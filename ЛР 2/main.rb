cdc1501cfc100789d6902e9776cdbc6d08f36e0c
Asya <111695385+asyanix@users.noreply.github.com>
2024-10-11 20:17:35 +0300

    Зад1.7. Добавление валидации для номера телефона

--- ЛР 2/main.rb
+++ ЛР 2/main.rb
@@ -1,9 +1,15 @@
 require_relative 'student_class.rb'
 
-student1 = Student.new({ id: 1, surname: "Cheuzh", name: "Asya", patronymic: "Aslanbievna", phone: "89827484999", telegram: "asyanix", email: "asya_cheuzh@gmail.com", git: "asyanix"})
-student2 = Student.new({id: 2, surname: "Kirkorov", name: "Philipp", patronymic: "Bedrosovich"})
-student3 = Student.new({id: 3, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "89728946758", telegram: "saintJuuuly"})
+begin
+  student1 = Student.new({ id: 1, surname: "Cheuzh", name: "Asya", patronymic: "Aslanbievna", phone: "89827484999", telegram: "asyanix", email: "asya_cheuzh@gmail.com", git: "asyanix"})
+  student1.show_info
+rescue ArgumentError => e
+  puts e.message
+end
 
-student1.show_info()
-student2.show_info()
-student3.show_info()
+begin
+  student3 = Student.new({id: 2, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "59728946758", telegram: "saintJuuuly"})
+  student1.show_info
+rescue ArgumentError => e
+  puts e.message
+end
--- ЛР 2/student_class.rb
+++ ЛР 2/student_class.rb
@@ -1,15 +1,29 @@
 class Student
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
+  # Сеттер для номера телефона
+  def phone=(value)
+    if value.nil? || Student.valid_phone?(value)
+      @phone = value
+    else
+      raise ArgumentError, "Invalid phone number: #{value} - for user #{self.surname} #{self.name} #{self.patronymic}"
+    end
+  end
+
   def initialize(params)
     @id = params[:id]
     @surname = params[:surname]
     @name = params[:name]
     @patronymic = params[:patronymic]
-    @phone = params[:phone] || nil
+    self.phone = params[:phone]
     @telegram = params[:telegram] || nil
     @email = params[:email] || nil
     @git = params[:git] || nil
+  end
+
+  # Проверки валидности номера телефона
+  def self.valid_phone?(phone)
+    phone.match?(/\A[78]9\d{9}\z/)
   end
 
   # Вывод информации о студенте
@@ -21,7 +35,7 @@ class Student
     puts "Phone: #{@phone}" if @phone
     puts "Telegram: #{@telegram}" if @telegram
     puts "Email: #{@email}" if @email
-    puts "Git : #{@git}" if @github
+    puts "Git : #{@git}" if @git
     puts ""
   end
 
