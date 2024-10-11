bf78128354ba4ce94a590bd38a24b90fdae6d21f
Asya <111695385+asyanix@users.noreply.github.com>
2024-10-11 18:51:19 +0300

    Зад1.3. Убрали геттеры и сеттеры для класса Student

--- ЛР 2/student_class.rb
+++ ЛР 2/student_class.rb
@@ -1,4 +1,5 @@
 class Student
+  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
   def initialize(id, surname, name, patronymic, phone = nil, telegram = nil, email = nil, git = nil)
       @id = id
@@ -9,78 +10,6 @@ class Student
       @telegram = telegram
       @email = email
       @git = git
-  end
-
-  # Геттер и сеттер для id
-  def get_id
-    @id
-  end
-
-  def set_id (value)
-    @id = value
-  end
-
-  # Геттер и сеттер для surname
-  def get_surname
-    @surname
-  end
-
-  def set_surname(value)
-    @surname = value
-  end
-  
-  # Геттер и сеттер для name
-  def get_name
-    @name
-  end
-
-  def set_name(value)
-    @name = value
-  end
-
-  # Геттер и сеттер для patronymic
-  def get_patronymic
-    @patronymic
-  end
-
-  def set_patronymic(value)
-    @patronymic = value
-  end
-
-  # Геттер и сеттер для phone
-  def get_phone
-    @phone
-  end
-
-  def set_phone(value)
-    @phone = value
-  end
-
-  # Геттер и сеттер для telegram
-  def get_telegram
-    @telegram
-  end
-
-  def set_telegram(value)
-    @telegram = value
-  end
-
-  # Геттер и сеттер для email
-  def get_email
-    @email
-  end
-
-  def set_email(value)
-    @email = value
-  end
-
-  # Геттер и сеттер для git
-  def get_git
-    @git
-  end
-
-  def set_git(value)
-    @git = value
   end
 
   # Вывод информации о студенте
