b78244b1812583fbac970e092e59ab59587fd0e2
Asya <111695385+asyanix@users.noreply.github.com>
2024-10-11 20:39:09 +0300

    Зад1.8. Добавление валидации для других строковых полей

--- ЛР 2/main.rb
+++ ЛР 2/main.rb
@@ -8,8 +8,15 @@ rescue ArgumentError => e
 end
 
 begin
-  student3 = Student.new({id: 2, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "59728946758", telegram: "saintJuuuly"})
-  student1.show_info
+  student2 = Student.new({id: 2, surname: "Nesvetaylov", name: "Vlad", patronymic: "Viktorovich", phone: "89728946758", telegram: "saintJuuuly", telegram: "saintJuuuly", email: "vlad_nesv@gmail.com", git: "святойИюююль"})
+  student2.show_info
+rescue ArgumentError => e
+  puts e.message
+end
+
+begin
+  student3 = Student.new(id: 4, surname: "Kuznetsov", name: "Dmitriy", patronymic: "Vladimirovich", git: "kuznetsoff", email: "kuznetsoff!gmail.com")
+  student3.show_info
 rescue ArgumentError => e
   puts e.message
 end
--- ЛР 2/student_class.rb
+++ ЛР 2/student_class.rb
@@ -3,10 +3,37 @@ class Student
 
   # Сеттер для номера телефона
   def phone=(value)
-    if value.nil? || Student.valid_phone?(value)
+    if Student.valid_phone?(value)
       @phone = value
     else
       raise ArgumentError, "Invalid phone number: #{value} - for user #{self.surname} #{self.name} #{self.patronymic}"
+    end
+  end
+
+  # Сеттер для тг
+  def telegram=(value)
+    if Student.valid_telegram?(value)
+      @telegram = value
+    else
+      raise ArgumentError, "Invalid telegram username: #{value} - for user #{self.surname} #{self.name} #{self.patronymic}"
+    end
+  end
+
+  # Сеттер для гита
+  def git=(value)
+    if Student.valid_git?(value)
+      @git = value
+    else
+      raise ArgumentError, "Invalid GitHub username: #{value} - for user #{self.surname} #{self.name} #{self.patronymic}"
+    end
+  end
+
+  # Сеттер для почты
+  def email=(value)
+    if Student.valid_email?(value)
+      @email = value
+    else
+      raise ArgumentError, "Invalid email: #{value} - for user #{self.surname} #{self.name} #{self.patronymic}"
     end
   end
 
@@ -16,14 +43,29 @@ class Student
     @name = params[:name]
     @patronymic = params[:patronymic]
     self.phone = params[:phone]
-    @telegram = params[:telegram] || nil
-    @email = params[:email] || nil
-    @git = params[:git] || nil
+    self.telegram = params[:telegram]
+    self.email = params[:email]
+    self.git = params[:git]
   end
 
-  # Проверки валидности номера телефона
+  # Проверка валидности номера телефона
   def self.valid_phone?(phone)
-    phone.match?(/\A[78]9\d{9}\z/)
+    phone.nil? || phone.match?(/\A[78]9\d{9}\z/)
+  end
+
+  # Проверка валидности тг
+  def self.valid_telegram?(telegram)
+    telegram.nil? || telegram.match?(/\A[a-zA-Z0-9_]+\z/)
+  end
+  
+  # Проверка валидности гита
+  def self.valid_git?(git)
+    git.nil? || git.match?(/\A[a-zA-Z0-9_]+\z/)
+  end
+  
+  # Проверка валидности почты
+  def self.valid_email?(email)
+    email.nil? || email.match?(/\A[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]+\z/)
   end
 
   # Вывод информации о студенте
