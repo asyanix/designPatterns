93f3953e4e9a591dce44d9ce262e95abac02d6c3
Asya <111695385+asyanix@users.noreply.github.com>
2024-12-13 23:35:50 +0300

    LR4.1.3: Add class Data_list, change name of attribute in Data_table class

---
+++ Student Authorization System/data_list.rb
@@ -0,0 +1,9 @@
+class Data_list
+  def initialize(list)
+    self.list = list
+  end
+  
+  private
+  
+  attr_accessor :list
+end
--- Student Authorization System/data_table.rb
+++ Student Authorization System/data_table.rb
@@ -1,36 +1,36 @@
 class Data_table
 
-  def initialize(data)
-    self.data = data
+  def initialize(table)
+    self.table = table
   end
 
   def get_by_index(row_index, column_index)
-    unless data.dig(row_index, column_index)
+    unless table.dig(row_index, column_index)
       raise IndexError, "Index is out of range" 
     end
-    data[row_index][column_index]
+    table[row_index][column_index]
   end 
 
   def row_count
-    data.size
+    table.size
   end
 
   def column_count
-    if data.empty? 
+    if table.empty? 
       return 0
     else
-      return data[0].size
+      return table[0].size
     end
   end
 
   private
 
-  attr_reader :data
+  attr_reader :table
   
-  def data=(data) 
-    unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
-      raise ArgumentError, 'Data must be a two-dimensional array.'
+  def table=(table) 
+    unless table.is_a?(Array) && table.all? { |row| row.is_a?(Array) }
+      raise ArgumentError, 'Table must be a two-dimensional array.'
     end
-    @data = data
+    @table = table
   end
 end
