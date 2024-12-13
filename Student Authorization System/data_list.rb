c4b095967cd58743d89f3c4a2bcb652a3aa5ea64
Asya <111695385+asyanix@users.noreply.github.com>
2024-12-14 00:28:42 +0300

    LR4.1.4 Add select, get_selected, get_names and  get_data methods in Data_list

--- Student Authorization System/data_list.rb
+++ Student Authorization System/data_list.rb
@@ -1,9 +1,36 @@
 class Data_list
   def initialize(list)
     self.list = list
+    @selected = []
   end
   
+  def select(index)
+    unless index.is_a?(Integer) && index.between?(0, list.size - 1)
+      raise ArgumentError, "Invalid index" 
+    end
+    @selected << index unless @selected.include?(index)
+  end
+
+  def get_selected
+    @selected.dup
+  end
+
+  def get_names
+    raise NotImplementedError, "This method should be implemented in a subclass"
+  end
+
+  def get_data
+    raise NotImplementedError, "This method should be implemented in a subclass"
+  end
+
   private
+
+  attr_reader :list
   
-  attr_accessor :list
+  def list=(list) 
+    unless list.is_a?(Array)
+      raise ArgumentError, 'List must be an array.'
+    end
+    @list = list
+  end
 end
