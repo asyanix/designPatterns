c74ab30a650fd1e79dab524f50f581e4e3b428f1
Asya <111695385+asyanix@users.noreply.github.com>
2025-01-03 18:47:34 +0300

    LR5: Develop an app containing 3 tabs

---
+++ Student Authorization System/app/main.rb
@@ -0,0 +1,26 @@
+require 'fox16'
+include Fox
+
+class StudentApp < FXMainWindow
+  def initialize(app)
+    super(app, "Students", width: 1024, height: 768)
+    tabs = FXTabBook.new(self, opts: LAYOUT_FILL)
+    FXTabItem.new(tabs, "Student List", nil)
+    student_list = FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
+    FXTabItem.new(tabs, "Tab 2", nil)
+    tab_2 = FXVerticalFrame.new(tabs, LAYOUT_FILL)
+    FXTabItem.new(tabs, "Tab 3", nil)
+    tab_3 = FXVerticalFrame.new(tabs)
+    FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
+  end
+
+  def create
+    super
+    show(PLACEMENT_SCREEN)
+  end
+end
+
+app = FXApp.new
+sv = StudentApp.new(app)
+app.create
+app.run
